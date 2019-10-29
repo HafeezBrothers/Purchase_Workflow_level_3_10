9fa3e34d82882fb53316b71491631d60247459e0
HafeezBrothers <mailto:hafeezbrothers.odoo@gmail.com>
2019-10-29 11:56:35 +0500

    My first File

---
+++ po_workflows_level_3/__manifest__.py
@@ -0,0 +1,20 @@
+{
+
+    'name':'Purchase Order WorkFlows (Level 3)',
+    'description' : 'PO WorkFlows',
+    'author' : 'Hafeez Brothers',
+    'version': '1.0',  
+    'depends' : [
+                   'base','purchase',
+                ],
+    'data' :[   
+                
+                'views/purchase_cus.xml',
+                'security/hr_user_rights1.xml'
+            ],
+    'installable' : True,
+    'price':50.00,
+    'currency':'EUR', 
+    
+
+}
